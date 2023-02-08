104ae8bdb049864b8157f0392c906dab111ad94d
phạm tú tài <93572750+phamtutai1338@users.noreply.github.com>
2023-02-08 11:32:53 +0700

    up

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = '';
+const giftUrl = 'http://nodemy.vn';
+const eventName = '사랑해요';
+const titleCard = 'LOVE';
+const contentCard = 'A great love is not one who loves many, but one who loves one woman for life';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'Thuong.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
